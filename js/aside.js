6d13888273c5b2d3a48b64bce16296d11840edbd
오건희 <oh98426@gmail.com>
2025-03-05 13:51:52 +0900

    공통 요소 정리 및 스크립트 추가

--- js/aside.js
+++ js/aside.js
@@ -5,12 +5,14 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 const asideToggleEvent = () => {
-  const lnbBar = document.querySelector(".lnb_bar__area"); // aside bar
+  const lnbBar = document.querySelector(".lnb_bar__area"); // aside bar (lnb)
+  const lnbCover = document.querySelector(".lnb_cover"); // 태블릿 & 모바일 메뉴 열렸을때 lnb 배경
 	const asidebutton = document.querySelector(".lnb_bar_control__box button"); // aside를 여는 버튼
 	const mobileOpenBtn = document.querySelector(".gnb_bar_mobile__area .mobile_menu_btn"); // 모바일 aside를 여는 버튼
 	const mobileCloseBtn = document.querySelector(".lnb_bar__area .btn_nav_close"); // 모바일 aside를 여는 버튼
   const contents = document.querySelector(".contents_container .contents"); // aside를 제외한 실제 컨텐츠 영역
-  const cover = document.querySelector(".contents_container .cover_bg"); // 태블릿 & 모바일 메뉴 열렸을때 배경
+  const saveBox = document.querySelector(".save__box"); // 뷰페이지 하단 save box
+
 	
   if (!lnbBar) return; // 요소가 없으면 실행하지 않음
 
@@ -18,30 +20,32 @@ const asideToggleEvent = () => {
   asidebutton.addEventListener("click", () => {
     if (lnbBar.classList.contains("fold")) {
       lnbBar.classList.remove("fold");
+      lnbCover.classList.add("on");
       contents.classList.remove("wide_mode");
-      cover.classList.add("on");
+      saveBox?.classList.remove("wide_mode");
     } else {
       lnbBar.classList.add("fold");
+      lnbCover.classList.remove("on");
       contents.classList.add("wide_mode");
-      cover.classList.remove("on");
+      saveBox?.classList.add("wide_mode");
     }
   });
 	
 	mobileOpenBtn.addEventListener("click", () => {
 		lnbBar.classList.remove("fold");
-		cover.classList.add("on");
+		lnbCover.classList.add("on");
 	});
 
 	mobileCloseBtn.addEventListener("click", () => { 
 		lnbBar.classList.add("fold");
-    cover.classList.remove("on");
+    lnbCover.classList.remove("on");
 	});	
 };
 
 const handleResize = () => {
 	const lnbBar = document.querySelector(".lnb_bar__area");
 	const contents = document.querySelector(".contents_container .contents");
-  const cover = document.querySelector(".contents_container .cover_bg");
+  const lnbCover = document.querySelector(".lnb_cover");
  
   if (!lnbBar) return;
 
@@ -51,14 +55,14 @@ const handleResize = () => {
 	if (mobile) {
 		lnbBar.classList.add("fold");
     contents.classList.remove("wide_mode");
-    cover.classList.remove("on");
+    lnbCover.classList.remove("on");
 	} else if (tablet) { 
 		lnbBar.classList.add("fold");
     contents.classList.add("wide_mode");
-    cover.classList.remove("on");
+    lnbCover.classList.remove("on");
 	}else {
     lnbBar.classList.remove("fold");
     contents.classList.remove("wide_mode");
-    cover.classList.add("on");
+    lnbCover.classList.add("on");
   }
 };
