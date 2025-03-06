64ffbca944de67711422b7a57718b8df461cd9c7
오건희 <oh98426@gmail.com>
2025-03-06 15:42:18 +0900

    공통 요소 snb lnb 수정

--- js/aside.js
+++ js/aside.js
@@ -13,13 +13,11 @@ const asideToggleEvent = () => {
   const contents = document.querySelector(".contents_container .contents"); // aside를 제외한 실제 컨텐츠 영역
   const saveBox = document.querySelector(".save__box"); // 뷰페이지 하단 save box
 
-	
   // 요소가 없으면 실행하지 않음
-  if (!lnbBar) return;
-  if (!asidebutton) return;
+  if (!lnbBar || !asidebutton) return;
 
 	// 클릭 이벤트 추가 (클래스 토글)	
-  asidebutton.addEventListener("click", () => {
+  asidebutton.addEventListener("click", () => {    
     if (lnbBar.classList.contains("fold")) {
       lnbBar.classList.remove("fold");
       lnbCover.classList.add("on");
@@ -33,49 +31,69 @@ const asideToggleEvent = () => {
     }
   });
 	
+  // 모바일 lnb 메뉴 오픈
 	mobileOpenBtn.addEventListener("click", () => {
 		lnbBar.classList.remove("fold");
-		lnbCover.classList.add("on");
+    lnbBar.classList.add("open");
+    lnbCover.classList.add("on");
 	});
 
+  // 모바일 lnb 메뉴 닫기
 	mobileCloseBtn.addEventListener("click", () => { 
-		lnbBar.classList.add("fold");
+    lnbBar.classList.add("fold");
+    lnbBar.classList.remove("open");
     lnbCover.classList.remove("on");
   });
 
+  // 백그라운드 눌렀을때 lnb 메뉴 닫기
   lnbCover.addEventListener("click", () => { 
     lnbBar.classList.add("fold");
     lnbCover.classList.remove("on");
     contents.classList.add("wide_mode");
     saveBox?.classList.add("wide_mode");
+    lnbBar.classList.remove("open");
   })
 };
 
+// 리사이즈 이벤트
 const handleResize = () => {
   const lnbBar = document.querySelector(".lnb_bar__area");
   const contents = document.querySelector(".contents_container .contents");
   const lnbCover = document.querySelector(".lnb_cover");
-  const saveBox = document.querySelector(".save__box"); // 뷰페이지 하단 save box
+  const saveBox = document.querySelector(".save__box");
+  const asidebutton = document.querySelector(".lnb_bar_control__box button");
+  const isMobile = window.innerWidth <= 640;
+  const isTablet = window.innerWidth <= 1280;
 
-  if (!lnbBar) return;
+  if (!lnbBar || !asidebutton) return;
 
-  const mobile = window.innerWidth <= 640;
-  const tablet = window.innerWidth <= 1280;
-
-  if (mobile) {
+  if (isMobile) {
     lnbBar.classList.add("fold");
+    lnbBar.classList.remove("open");
     contents.classList.remove("wide_mode");
-    // saveBox?.classList.add("wide_mode");
     lnbCover.classList.remove("on");
-  } else if (tablet) {
+  } else if (isTablet) {
     lnbBar.classList.add("fold");
+    lnbBar.classList.remove("open");
     contents.classList.add("wide_mode");
-    // saveBox?.classList.add("wide_mode");
     lnbCover.classList.remove("on");
   } else {
     lnbBar.classList.remove("fold");
+    lnbBar.classList.remove("open");
     contents.classList.remove("wide_mode");
     saveBox?.classList.remove("wide_mode");
     lnbCover.classList.add("on");
+  }
+
+  asidebutton.addEventListener("click", toggleLnb);
+};
+
+// 태블릿 lnb 메뉴 오픈 토글
+const toggleLnb = () => {
+  const lnbBar = document.querySelector(".lnb_bar__area");
+  const isTablet = window.innerWidth <= 1280;
+
+  if (isTablet) {
+    lnbBar.classList.toggle("open");
   }
 };
